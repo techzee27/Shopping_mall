c36f3815ccaf155ac22489d5132d4e2d34de4d5a
Mohammed Zeeshan <techzee2723@gmail.com>
2026-01-17 21:58:30 +0530

    crucial updates

---
+++ mall/src/main/java/com/mall/management/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.mall.management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOrigins("*") // Allow all origins (change to specific domains in production)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
